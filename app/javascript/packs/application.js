c09c9a1f5e13832b81526d23af70fe7c70750f8e
Dan Kim <itsdanya@gmail.com>
2020-02-05 02:24:03 +0300

    Initialize Vue in application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,17 @@ import '../css/application.css'
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+// Vue
+
+import Vue from "vue";
+import App from "../app.vue";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const app = new Vue({
+    render: h => h(App)
+  }).$mount();
+  document.body.appendChild(app.$el);
+
+  console.log(app);
+});
