ee692ee4aed8f2d1cc71842139e1248e099fd2ed
Dan Kim <itsdanya@gmail.com>
2020-02-05 21:13:42 +0300

    Add SectionArticles

---
+++ app/javascript/components/pages/home/SectionArticles.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="articles" style="background-color: #fbfbfc">
+    <div class="container py-12">
+      <h2 class="heading-1 text-center" style="margin-bottom: 2rem;">Articles</h2>
+
+      <p class="md:max-w-md mx-auto text-center mb-12 text-lg font-light leading-normal">“We are not taught how to learn in school, we are taught how to pass tests. The spacing effect is a far more effective way to learn and retain information that works with our brain instead of against it.”</p>
+
+      <div class="flex flex-col md:flex-row">
+        <a href="https://qz.com/1211561/how-to-learn-a-language-use-spaced-repetition/" target="_blank" class="article md:flex-1 flex flex-col rounded-lg overflow-hidden md:mx-4 mb-8 md:mb-0 hover:shadow-lg">
+          <img src="@images/article-01.jpg" alt="" class="object-cover">
+
+          <div class="flex-grow flex flex-col p-5 bg-white">
+            <span class="text-grey-dark text-sm mb-3">Feb 23, 2018 — Quartz</span>
+
+            <h3 class="font-medium text-xl mb-3">The scientific, efficient way to learn languages: “spaced repetition”</h3>
+
+            <p class="text-grey-darker font-light leading-normal">Learning a language can be frustrating. Progress often feels slow. Part of that is because there is a lot to learn. But part of it is also that language education often does not utilize the best methods out there.</p>
+          </div>
+        </a>
+        <a href="https://www.theguardian.com/education/2016/jan/23/spaced-repetition-a-hack-to-make-your-brain-store-information" target="_blank" class="article md:flex-1 flex flex-col rounded-lg overflow-hidden md:mx-4 mb-8 md:mb-0 hover:shadow-lg">
+          <img src="@images/article-02.jpg" alt="" class="object-cover">
+
+          <div class="flex-grow flex flex-col p-5 bg-white">
+            <span class="text-grey-dark text-sm mb-3">Jan 23, 2016 — The Guardian</span>
+
+            <h3 class="font-medium text-xl mb-3">Spaced repetition: a hack to make your brain store information </h3>
+
+            <p class="text-grey-darker font-light leading-normal">The way we think about health has been revolutionised by our growing evidence of how the body works. Yet the way most of us approach learning remains the same as it has for centuries.</p>
+          </div>
+        </a>
+        <a href="https://fs.blog/2018/12/spacing-effect/" target="_blank" class="article md:flex-1 flex flex-col rounded-lg overflow-hidden md:mx-4 mb-8 md:mb-0 hover:shadow-lg">
+          <img src="@images/article-03.png" alt="" class="object-cover">
+
+          <div class="flex-grow flex flex-col p-5 bg-white">
+            <span class="text-grey-dark text-sm mb-3">Dec 18, 2018 — Farnam Street</span>
+
+            <h3 class="font-medium text-xl mb-3">The Spacing Effect: How to Improve Learning and Maximize Retention</h3>
+
+            <p class="text-grey-darker font-light leading-normal">We are not taught how to learn in school, we are taught how to pass tests. The spacing effect is a far more effective way to learn and retain information that works with our brain instead of against it.</p>
+          </div>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
--- app/javascript/components/pages/home/index.vue
+++ app/javascript/components/pages/home/index.vue
@@ -4,6 +4,7 @@
     <SectionSpacedRepetition />
     <SectionHowTo />
     <SectionFeatures />
+    <SectionArticles />
   </div>
 </template>
 
@@ -12,9 +13,15 @@ import SectionHero from './SectionHero'
 import SectionSpacedRepetition from './SectionSpacedRepetition'
 import SectionHowTo from './SectionHowTo'
 import SectionFeatures from './SectionFeatures'
+import SectionArticles from './SectionArticles'
 
 export default {
-  components: { SectionHero, SectionSpacedRepetition, SectionFeatures }
+  components: {
+    SectionHero,
+    SectionSpacedRepetition,
+    SectionFeatures,
+    SectionArticles
+  }
 }
 </script>
 
