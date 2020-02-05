b4e92f183c66b57e40a39793d600d507e9a90cbf
Dan Kim <itsdanya@gmail.com>
2020-02-05 20:26:58 +0300

    Add hero section to home page

--- app/javascript/components/pages/home.vue
+++ app/javascript/components/pages/home.vue
@@ -1,6 +1,24 @@
 <template>
-  <div>
-    <router-link to="/">keeek</router-link>
+  <div class="container">
+    <img src="@images/home.svg" alt="Hero" class="block mx-auto mb-8 md:mb-10 max-w-md w-full">
+
+    <div class="flex flex-col items-center text-center mb-6">
+      <h1 class="mb-8 md:text-4xl lg:text-5xl max-w-md">Memorize everything with flashcards</h1>
+
+      <p class="mb-8 text-xl font-light leading-normal max-w-md">Spacecards uses spaced repetition to help you memorize all kinds of information: words, exam questions, formulas, quotes, etc. Bring your own content—there’s no limit.</p>
+
+      <router-link to="/" class="btn btn-primary btn-lg tracking-wide mb-8">
+        Start learning
+      </router-link>
+
+      <p class="text-sm text-grey-darkest flex items-center">
+        <span class="mx-2">Sign in with</span>
+
+        <router-link to="/" class="mx-3">
+          <img src="@images/icon-google.svg" alt="Sign in with Google">
+        </router-link>
+      </p>
+    </div>
   </div>
 </template>
 
