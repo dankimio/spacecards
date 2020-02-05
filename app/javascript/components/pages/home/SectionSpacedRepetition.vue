df0880b52d18a9d0045ecbdef2082b941361c9a9
Dan Kim <itsdanya@gmail.com>
2020-02-05 23:37:26 +0300

    Adjust images

--- app/javascript/components/pages/home/SectionSpacedRepetition.vue
+++ app/javascript/components/pages/home/SectionSpacedRepetition.vue
@@ -4,22 +4,22 @@
       <h2 class="heading-1 text-center" style="margin-bottom: 2rem;">How it works</h2>
 
       <div class="flex flex-col lg:flex-row">
-        <div class="lg:w-2/5 mb-6 lg:mb-0">
-          <p class="text-lg text-grey-darkest font-light leading-normal mb-6">
-            Start by creating flashcards from your content. These can be words from a book you’re reading, questions for your college exam, lines from a script you’re rehearsing, etc.
+        <div class="lg:w-1/2 mb-6 lg:mb-0 lg:pr-10">
+          <p class="text-lg text-gray-800 font-light leading-normal mb-6">
+            Start by creating flashcards from your content. These can be words from a book you’re reading, college exam questions, lines from a script, etc.
           </p>
 
-          <p class="text-lg text-grey-darkest font-light leading-normal mb-6">
-            Spacecards will then plan your study sessions for each day, in a way that will help you memorize the information—by spreading out material with intervals that increase over time as you progress.
+          <p class="text-lg text-gray-800 font-light leading-normal mb-6">
+            Spacecards will plan your study sessions for each day, in a way that will help you memorize the information—by spreading out material with intervals that increase over time as you progress.
           </p>
 
-          <p class="text-lg text-grey-darkest font-light leading-normal mb-6">
+          <p class="text-lg text-gray-800 font-light leading-normal mb-6">
             Based on your performance, Spacecards will schedule future sessions. The better you perform, the longer the intervals will be.
           </p>
         </div>
 
-        <div class="lg:w-3/5">
-          <img src="@images/chart.svg" alt="Spaced repetition chart" class="block lg:ml-auto">
+        <div class="max-w-lg lg:w-1/2 self-center">
+          <img src="@images/chart.png" alt="Spaced repetition chart" class="block">
         </div>
       </div>
     </div>
