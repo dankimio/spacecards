117c6ad0811575c86fa3f9a65f3e276c0ad6b7dd
Dan Kim <itsdanya@gmail.com>
2020-02-05 23:16:05 +0300

    Clean up environment.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,12 +1,13 @@
-const path = require("path");
+const path = require('path')
 const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
 
+// Custom configuration: aliases
 environment.config.resolve.alias = {
   '@': path.resolve(__dirname, '../../app/javascript'),
   '@images': path.resolve(__dirname, '../../app/javascript/images')
-};
+}
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
