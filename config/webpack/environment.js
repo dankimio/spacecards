a9fcd7ee0e6b42593c0762b7ebba7605c53a5fb6
Dan Kim <itsdanya@gmail.com>
2020-02-05 23:15:36 +0300

    Revert "Configure postcss-loader"
    e9bce5b6a0f3d4be278972390792cf470840fffa

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,26 +1,13 @@
-const path = require('path')
+const path = require("path");
 const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
 
-// Custom configuration: aliases
 environment.config.resolve.alias = {
   '@': path.resolve(__dirname, '../../app/javascript'),
   '@images': path.resolve(__dirname, '../../app/javascript/images')
-}
-
-// Custom configuration: configure PostCSS loader
-const postcssLoader = {
-  test: /\.postcss$/,
-  use: [
-    'vue-style-loader',
-    { loader: 'postcss-loader', options: { importLoaders: 1 } },
-    'postcss-loader'
-  ]
-}
-environment.loaders.append('postcss', postcssLoader)
+};
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
-
 module.exports = environment
