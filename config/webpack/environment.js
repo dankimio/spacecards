73fa67824bb71b8996eaff3cc30035d02ef9f507
Dan Kim <itsdanya@gmail.com>
2020-02-05 20:24:22 +0300

    Set up Webpack aliases

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,12 @@
+const path = require("path");
 const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
+
+environment.config.resolve.alias = {
+  '@': path.resolve(__dirname, '../../app/javascript'),
+  '@images': path.resolve(__dirname, '../../app/javascript/images')
+};
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
