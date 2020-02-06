3eeb2cfd1170cf740c07aa1c185e95a3eeaa1b71
Dan Kim <itsdanya@gmail.com>
2020-02-06 23:32:24 +0300

    Override SessionsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   root 'pages#index'
+
+  scope :api do
+    devise_for :users, controllers: {
+      sessions: 'users/sessions'
+    }
+  end
+
+  namespace :api do
+  end
 end
