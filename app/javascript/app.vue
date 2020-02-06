e6baf383ad42a8c69a71ed361d440ae738e1c93e
Dan Kim <itsdanya@gmail.com>
2020-02-06 15:06:10 +0300

    Add Footer

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -2,14 +2,16 @@
   <div>
     <Header />
     <router-view></router-view>
+    <Footer />
   </div>
 </template>
 
 <script>
 import Header from '@/components/header'
+import Footer from '@/components/footer'
 
 export default {
-  components: { Header }
+  components: { Header, Footer }
 }
 </script>
 
---
+++ app/javascript/components/footer/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer class="container mt-auto flex justify-between pt-6 pb-8 lg:pt-10 lg:pb-16">
+    <div class="text-gray-800">
+      Made by <a href="https://dankim.io" target="_blank">@dankimio</a>
+    </div>
+
+    <div class="text-grey">
+      <ul class="list-reset">
+        <li class="inline-block ml-4">
+          <a href="#" class="text-grey">About</a>
+        </li>
+      </ul>
+    </div>
+  </footer>
+</template>
