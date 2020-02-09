5f29cc2108a05c4a59c9e757a27a01782ad58302
Dan Kim <itsdanya@gmail.com>
2020-02-10 01:53:27 +0300

    Extract card component

---
+++ app/javascript/components/card.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="flex flex-col lg:flex-row border-2 border-gray-200 px-4 py-4 mb-3 rounded">
+    <div class="w-1/3">
+      {{ card.front }}
+    </div>
+
+    <hr class="border-b-2 border-gray-100 my-3">
+
+    <div class="w-2/3 mb-4 lg:mb-0">
+      {{ card.back }}
+    </div>
+    <div class="flex flex-shrink-0">
+      <a href="" class="flex items-center text-gray-500 hover:text-gray-800 mr-5">
+        <inline-svg
+          :src="require('@images/icon-pencil.svg')"
+          class="inline-block w-4 h-4 mr-1 stroke-current"
+        />
+        <span>Edit</span>
+      </a>
+
+      <a href="" class="flex items-center text-gray-500 hover:text-gray-800">
+        <inline-svg
+          :src="require('@images/icon-trash.svg')"
+          class="inline-block w-4 h-4 mr-1 fill-current"
+        />
+        <span>Delete</span>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['card']
+}
+</script>
--- app/javascript/views/cards/index.vue
+++ app/javascript/views/cards/index.vue
@@ -8,41 +8,16 @@
       Add cards
     </router-link>
 
-    <div v-for="card in cards" v-bind:key="card.id"
-      class="flex flex-col lg:flex-row border-2 border-gray-200 px-4 py-4 mb-3 rounded"
-    >
-      <div class="w-1/3">
-        {{ card.front }}
-      </div>
-
-      <hr class="border-b-2 border-gray-100 my-3">
-
-      <div class="w-2/3 mb-4 lg:mb-0">
-        {{ card.back }}
-      </div>
-      <div class="flex flex-shrink-0">
-        <a href="" class="flex items-center text-gray-500 hover:text-gray-800 mr-5">
-          <inline-svg
-            :src="require('@images/icon-pencil.svg')"
-            class="inline-block w-4 h-4 mr-1 stroke-current"
-          />
-          <span>Edit</span>
-        </a>
-
-        <a href="" class="flex items-center text-gray-500 hover:text-gray-800">
-          <inline-svg
-            :src="require('@images/icon-trash.svg')"
-            class="inline-block w-4 h-4 mr-1 fill-current"
-          />
-          <span>Delete</span>
-        </a>
-      </div>
-    </div>
+    <Card v-for="card in cards"
+      v-bind:key="card.id" v-bind:card="card" />
   </div>
 </template>
 
 <script>
+import Card from '@/components/card'
+
 export default {
+  components: { Card },
   data() {
     return {
       cards: [
