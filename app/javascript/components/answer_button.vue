9f8a887077d274cbc8ec412a640a7ab3ec750485
Dan Kim <itsdanya@gmail.com>
2020-02-10 20:37:26 +0300

    Extract AnswerButton component

---
+++ app/javascript/components/answer_button.vue
@@ -0,0 +1,87 @@
+<template>
+  <button
+    class="flex md:flex-1 items-center justify-center
+    mb-3 md:mb-0 w-full py-2 rounded-md
+    border-2 border-gray-200
+    transition duration-200
+    transform hover:-translate-y-1"
+    v-bind:class="states[answer].class"
+  >
+    <span class="hover:text-white mr-3">
+      {{ states[answer].label }}
+    </span>
+    <inline-svg class="fill-current"
+      :src="states[answer].icon" />
+  </button>
+</template>
+
+<script>
+const states = {
+  'bad': {
+    label: 'Bad',
+    class: 'answer-button--bad',
+    icon: require('../images/icon-btn-bad.svg'),
+  },
+  'okay': {
+    label: 'Okay',
+    class: 'answer-button--okay',
+    icon: require('../images/icon-btn-okay.svg'),
+  },
+  'good': {
+    label: 'Good',
+    class: 'answer-button--good',
+    icon: require('../images/icon-btn-good.svg'),
+  },
+  'excellent': {
+    label: 'Excellent',
+    class: 'answer-button--excellent',
+    icon: require('../images/icon-btn-excellent.svg'),
+  },
+}
+
+export default {
+  props: ['answer'],
+  data() {
+    return { states: states }
+  }
+}
+</script>
+
+<style scoped>
+.answer-button--bad {
+  &:hover {
+    @apply .bg-red-500 .border-red-500;
+    & span { @apply .text-white; }
+    & svg { @apply .text-white }
+  }
+  & span { @apply .text-red-500; }
+  & svg { @apply .text-red-500; }
+}
+.answer-button--okay {
+  &:hover {
+    @apply .bg-orange-500 .border-orange-500;
+    & span { @apply .text-white; }
+    & svg { @apply .text-white }
+  }
+  & span { @apply .text-orange-500; }
+  & svg { @apply .text-orange-500; }
+}
+.answer-button--good {
+  &:hover {
+    @apply .bg-blue-500 .border-blue-500;
+    & span { @apply .text-white; }
+    & svg { @apply .text-white }
+  }
+  & span { @apply .text-blue-500; }
+  & svg { @apply .text-blue-500; }
+}
+.answer-button--excellent {
+  &:hover {
+    @apply .bg-green-500 .border-green-500;
+    & span { @apply .text-white; }
+    & svg { @apply .text-white }
+  }
+  & span { @apply .text-green-500; }
+  & svg { @apply .text-green-500; }
+}
+</style>
--- app/javascript/views/study_session/index.vue
+++ app/javascript/views/study_session/index.vue
@@ -1,56 +1,21 @@
 <template>
   <div class="container flex flex-col mx-auto max-w-4xl px-6 mb-64">
-    <Card />
+    <Card class="mb-4" />
 
-    <div class="flex flex-col sm:flex-col md:flex-row justify-between items-center my-5">
-      <button
-        class="flex md:flex-1 items-center justify-center
-        mb-3 md:mb-0 w-full py-2 rounded-md
-        border-2 border-gray-200 hover:border-red-500
-        hover:bg-red-500 transition duration-200
-        transform hover:-translate-y-1"
-      >
-        <span class="text-red-500 group-hover:text-white mr-3">Bad</span>
-        <img class="icon-btn" alt="Sad icon" src="@images/icon-btn-bad.svg">
-      </button>
-      <button
-        class="flex md:flex-1 items-center justify-center
-        mb-3 md:mb-0 w-full py-2 rounded-md
-        border-2 border-gray-200 hover:border-orange-500
-        hover:bg-orange-500 transition duration-200
-        transform hover:-translate-y-1"
-      >
-        <span class="text-orange-500 group-hover:text-white mr-3">Okay</span>
-        <img class="icon-btn" alt="Okay icon" src="@images/icon-btn-okay.svg">
-      </button>
-      <button
-        class="flex md:flex-1 items-center justify-center
-        mb-3 md:mb-0 w-full py-2 rounded-md
-        border-2 border-gray-200 hover:border-blue-500
-        hover:bg-blue-500 transition duration-200
-        transform hover:-translate-y-1"
-      >
-        <span class="text-blue-500 group-hover:text-white mr-3">Good</span>
-        <img class="icon-btn" alt="Good icon" src="@images/icon-btn-good.svg">
-      </button>
-      <button
-        class="flex md:flex-1 items-center justify-center
-        mb-3 md:mb-0 w-full py-2 rounded-md
-        border-2 border-gray-200 hover:border-green-500
-        hover:bg-green-500 transition duration-200
-        transform hover:-translate-y-1"
-      >
-        <span class="text-green-500 group-hover:text-white mr-3">Excellent</span>
-        <img class="icon-btn" alt="Excellent icon" src="@images/icon-btn-excellent.svg">
-      </button>
+    <div class="flex flex-col sm:flex-col md:flex-row justify-between items-center mb-4">
+      <AnswerButton answer="bad" />
+      <AnswerButton answer="okay" />
+      <AnswerButton answer="good" />
+      <AnswerButton answer="excellent" />
     </div>
   </div>
 </template>
 
 <script>
 import Card from './card'
+import AnswerButton from '@/components/answer_button'
 
 export default {
-  components: { Card }
+  components: { Card, AnswerButton }
 }
 </script>
