34231aad78b862996b9ccad144c6afd6e9d096cf
Dan Kim <itsdanya@gmail.com>
2020-02-10 20:46:54 +0300

    Add margins between answer buttons

--- app/javascript/views/study_session/index.vue
+++ app/javascript/views/study_session/index.vue
@@ -3,10 +3,10 @@
     <Card class="mb-4" />
 
     <div class="flex flex-col sm:flex-col md:flex-row justify-between items-center mb-4">
-      <AnswerButton answer="bad" />
-      <AnswerButton answer="okay" />
-      <AnswerButton answer="good" />
-      <AnswerButton answer="excellent" />
+      <AnswerButton answer="bad" class="mr-0 md:mr-2 lg:mr-3" />
+      <AnswerButton answer="okay" class="mr-0 md:mr-2 lg:mr-3" />
+      <AnswerButton answer="good" class="mr-0 md:mr-2 lg:mr-3" />
+      <AnswerButton answer="excellent" class="mr-0" />
     </div>
   </div>
 </template>
