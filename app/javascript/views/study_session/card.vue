c446fa124b294ecd9da0ec25015021365412fb0f
Dan Kim <itsdanya@gmail.com>
2020-02-10 20:59:45 +0300

    Fix heading layout on mobile

--- app/javascript/views/study_session/card.vue
+++ app/javascript/views/study_session/card.vue
@@ -1,23 +1,5 @@
 <template>
   <div>
-    <div class="flex flex-row items-baseline">
-      <h1 class="text-3xl font-bold">World Capitals</h1>
-      <span class="text-3xl mx-3 font-light">→</span>
-      <h2 class="text-3xl font-light">Study</h2>
-    </div>
-
-    <div class="mt-3 flex flex-row justify-between">
-      <div class="flex flex-row">
-        <span class="antialiased text-gray-600 font-medium">World</span>
-      </div>
-
-      <div class="flex flex-row">
-        <span class="antialiased text-3xl mr-2 leading-none">12</span>
-        <span class="antialiased text-sm font-medium text-gray-600">
-          Cards left
-        </span>
-      </div>
-    </div>
     <div class="flex flex-col justify-center items-center w-full bg-gray-100 rounded my-3">
       <span class="antialiased font-semibold text-4xl py-4">Zimbabwe</span>
       <hr class="divider w-11/12 text-md text-gray-200 font-bold border">
