c87aa1e254632c10731bedfbf88b9530bc7cb31d
Dan Kim <itsdanya@gmail.com>
2020-02-14 22:29:17 +0300

    Fix margins in the header

--- app/javascript/components/header.vue
+++ app/javascript/components/header.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container flex flex-col sm:flex-row sm:justify-between sm:items-center py-8">
-    <div class="text-4xl mb-3 sm:mb-0">
+    <div class="text-4xl mb-4 sm:mb-0">
       <router-link to="/">
         <img src="@images/logo.png" alt="Logo" class="h-8">
       </router-link>
@@ -9,20 +9,20 @@
       <router-link
         v-if="!$store.getters.loggedIn"
         to="/log-in"
-        class="ml-5"
+        class="mr-6 md:mr-0 md:ml-10"
       >
         Log in
       </router-link>
       <router-link
         to="/explore"
-        class="ml-5"
+        class="mr-6 md:mr-0 md:ml-10"
       >
         Explore
       </router-link>
       <router-link
         v-if="$store.getters.loggedIn"
         to="/decks"
-        class="ml-5"
+        class="mr-6 md:mr-0 md:ml-10"
       >
         Decks
       </router-link>
