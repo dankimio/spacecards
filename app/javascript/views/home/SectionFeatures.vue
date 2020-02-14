96a562bc2d2fed9302865c6d2775b69cef3e3efe
Dan Kim <itsdanya@gmail.com>
2020-02-15 02:12:13 +0300

    Add Markdown feature

--- app/javascript/views/home/SectionFeatures.vue
+++ app/javascript/views/home/SectionFeatures.vue
@@ -5,17 +5,16 @@
     </h2>
 
     <div id="features" class="container mb-16">
-      <div class="flex flex-col max-w-sm mx-auto text-center">
-        <img
-          src="@images/home/icon-features-01.svg"
-          alt="Markdown support"
-          class="block mb-6"
-        >
-        <h3 class="font-medium mb-6">
+      <div class="flex flex-col items-center max-w-sm mx-auto text-center">
+        <inline-svg
+          :src="require('@images/home/logo-markdown.svg')"
+          class="w-24 h-24 text-gray-700 fill-current mb-2"
+        />
+        <h3 class="text-xl font-medium mb-6">
           Markdown support
         </h3>
         <p class="font-light text-gray-800 leading-normal">
-          Use Markdown to emphasize key elements, create lists and add formatting to your flashcards.
+          Use Markdown to add basic formatting to your flashcards: highlight elements, create lists.
         </p>
       </div>
 
