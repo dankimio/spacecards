14412986b407fcba8a7144125cabdf7845d22bfa
Dan Kim <itsdanya@gmail.com>
2020-03-06 00:58:17 +0800

    Add decks and cards to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,11 @@ user = User.create!(
   email: 'foo@bar.com',
   password: 'qwerty123'
 )
+
+10.times do |i|
+  deck = Deck.create!(name: "Deck #{i + 1}", user: user)
+
+  # 10.times do |j|
+  #   deck.cards.create!(front: "Front #{j + 1}", back: "Back #{j + 1}")
+  # end
+end
