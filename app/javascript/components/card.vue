fe37b33f061aeff5471330f7bc6b3d4eeea82c84
Dan Kim <itsdanya@gmail.com>
2020-03-06 03:00:17 +0800

    Update Card layout

--- app/javascript/components/card.vue
+++ app/javascript/components/card.vue
@@ -1,76 +1,79 @@
 <template>
-  <div class="flex flex-col lg:flex-row lg:items-start border border-gray-200 px-4 py-4 mb-3 rounded">
-    <div class="lg:w-2/5 lg:pr-2">
-      <div v-if="!isEditing" class="text-gray-900" v-text="front" />
+  <div class="flex flex-col border mb-4 rounded">
+    <div class="flex flex-col">
+      <div class="px-4 py-4">
+        <div v-if="!isEditing" class="text-gray-900 text-lg" v-text="front" />
 
-      <textarea
-        v-if="isEditing"
-        v-model="front"
-        name="front"
-        class="form-control w-full"
-        autofocus
-      />
+        <textarea
+          v-if="isEditing"
+          v-model="front"
+          name="front"
+          class="form-control w-full"
+          autofocus
+        />
+      </div>
+
+      <hr class="border-gray-200">
+
+      <div class="px-4 py-4">
+        <div v-if="!isEditing" class="text-gray-900 text-lg" v-text="back" />
+
+        <textarea
+          v-if="isEditing"
+          v-model="back"
+          name="back"
+          class="form-control w-full"
+        />
+      </div>
     </div>
+    <div class="bg-gray-100 text-gray-600 text-sm px-4 py-2">
+      <div v-if="!isEditing" class="lg:w-48 flex">
+        <button
+          class="flex items-center text-gray-500 hover:text-gray-800 mr-5"
+          @click="toggleEdit"
+        >
+          <inline-svg
+            :src="require('@images/icon-pencil.svg')"
+            class="inline-block w-4 h-4 mr-1 stroke-current"
+          />
+          <span>Edit</span>
+        </button>
 
-    <hr class="border-b-2 border-gray-100 my-3">
-
-    <div class="lg:w-3/5 mb-4 lg:px-2 lg:mb-0 text-gray-900">
-      <div v-if="!isEditing" class="text-gray-900" v-text="back" />
-
-      <textarea
-        v-if="isEditing"
-        v-model="back"
-        name="back"
-        class="form-control w-full"
-      />
-    </div>
-
-    <div v-if="!isEditing" class="lg:w-48 flex lg:pl-2">
-      <button
-        class="flex items-center text-gray-500 hover:text-gray-800 mr-5"
-        @click="toggleEdit"
-      >
-        <inline-svg
-          :src="require('@images/icon-pencil.svg')"
-          class="inline-block w-4 h-4 mr-1 stroke-current"
-        />
-        <span>Edit</span>
-      </button>
-
-      <button
-        class="flex items-center text-gray-500 hover:text-red-700"
-        @click="destroy"
-      >
-        <inline-svg
-          :src="require('@images/icon-trash.svg')"
-          class="inline-block w-4 h-4 mr-1 fill-current"
-        />
-        <span>Delete</span>
-      </button>
-    </div>
-    <div v-if="isEditing" class="lg:w-48 flex lg:pl-2">
-      <button
-        v-if="isEditing"
-        class="flex items-center text-gray-500 hover:text-gray-800 mr-3"
-        @click="toggleEdit"
-      >
-        <inline-svg
-          :src="require('@images/icon-checkmark.svg')"
-          class="inline-block w-4 h-4 mr-1 stroke-current"
-        />
-        <span>Save</span>
-      </button>
-      <button
-        v-if="isEditing"
-        class="flex items-center text-gray-500 hover:text-gray-800"
-        @click="toggleEdit"
-      >
-        <inline-svg
-          :src="require('@images/icon-close.svg')"
-          class="inline-block w-4 h-4 mr-1 fill-current"
-        />
-        <span>Cancel</span>
-      </button>
+        <button
+          class="flex items-center text-gray-500 hover:text-red-700"
+          @click="destroy"
+        >
+          <inline-svg
+            :src="require('@images/icon-trash.svg')"
+            class="inline-block w-4 h-4 mr-1 fill-current"
+          />
+          <span>Delete</span>
+        </button>
+      </div>
+      <div v-if="isEditing" class="lg:w-48 flex">
+        <button
+          v-if="isEditing"
+          class="flex items-center text-gray-500 hover:text-gray-800 mr-3"
+          @click="toggleEdit"
+        >
+          <inline-svg
+            :src="require('@images/icon-checkmark.svg')"
+            class="inline-block w-4 h-4 mr-1 stroke-current"
+          />
+          <span>Save</span>
+        </button>
+        <button
+          v-if="isEditing"
+          class="flex items-center text-gray-500 hover:text-gray-800"
+          @click="toggleEdit"
+        >
+          <inline-svg
+            :src="require('@images/icon-close.svg')"
+            class="inline-block w-4 h-4 mr-1 fill-current"
+          />
+          <span>Cancel</span>
+        </button>
+      </div>
     </div>
   </div>
 </template>
