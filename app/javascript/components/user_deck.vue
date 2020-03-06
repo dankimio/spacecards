cc4689a6b56d1ca8c1c20897749b2013fdc9dce5
Dan Kim <itsdanya@gmail.com>
2020-03-06 17:24:43 +0800

    Update UserDeck layout

--- app/javascript/components/user_deck.vue
+++ app/javascript/components/user_deck.vue
@@ -1,20 +1,26 @@
 <template>
   <div
-    class="flex flex-col md:flex-row justify-between mb-5 py-5 px-5 bg-gray-100 rounded duration-150 transform hover:-translate-y-1 hover:shadow-lg"
+    class="flex flex-col justify-between mb-5 border rounded duration-100 transform hover:shadow-lg"
   >
-    <div class="mb-2 md:mb-0">
+    <div class="py-4 px-5">
       <div class="text-lg md:mb-2">
         <router-link to="/study-session">
           {{ deck.name }}
         </router-link>
       </div>
-      <router-link to="#" class="text-sm text-gray-500 hover:text-gray-800">
+      <router-link :to="`/decks/${deck.id}`" class="flex items-center text-sm text-gray-500 hover:text-indigo-600 flex-shrink-0 mr-2">
+        <inline-svg
+          :src="require('@images/icon-albums.svg')"
+          class="w-4 h-4 mr-1 fill-current"
+        />
         {{ deck.cardsCount }} cards
       </router-link>
     </div>
 
-    <div class="flex-shrink-0 md:text-right">
-      <router-link to="/study-session" class="inline-flex items-center mr-5 text-gray-500 hover:text-indigo-600">
+    <hr>
+
+    <div class="flex text-sm bg-gray-100 py-2 px-5">
+      <router-link to="/study-session" class="flex items-center mr-5 text-gray-500 hover:text-indigo-600">
         <inline-svg
           :src="require('@images/icon-play.svg')"
           class="inline-block w-4 h-4 mr-1 fill-current"
@@ -22,7 +28,7 @@
         Study
       </router-link>
 
-      <router-link to="/cards" class="inline-flex items-center mr-5 text-gray-500 hover:text-indigo-600">
+      <router-link :to="`/decks/${deck.id}`" class="flex items-center mr-5 text-gray-500 hover:text-indigo-600">
         <inline-svg
           :src="require('@images/icon-list.svg')"
           class="inline-block w-4 h-4 mr-1 stroke-current"
@@ -30,7 +36,7 @@
         Browse
       </router-link>
 
-      <router-link to="/decks/1/edit" class="inline-flex items-center text-gray-500 hover:text-indigo-600">
+      <router-link to="/decks/1/edit" class="flex items-center text-gray-500 hover:text-indigo-600">
         <inline-svg
           :src="require('@images/icon-settings.svg')"
           class="inline-block w-4 h-4 mr-1 fill-current"
