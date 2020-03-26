42ab64d329091b18e233baf1609c3e566e24fb87
Dan Kim <itsdanya@gmail.com>
2020-03-26 20:04:14 +0300

    Add associations

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -3,6 +3,7 @@ class Deck < ApplicationRecord
 
   # TODO: switch dependent to :restrict_with_exception
   has_many :cards, dependent: :destroy
+  has_many :user_decks, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 50 }
 end
