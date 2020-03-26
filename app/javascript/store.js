79f0298d822bd4a66b7afe92ec71d776e4437ae5
Dan Kim <itsdanya@gmail.com>
2020-03-26 21:39:31 +0300

    Vuex: extract users module

--- app/javascript/store.js
+++ app/javascript/store.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import axios from 'axios'
 
 import cards from './store/modules/cards'
 import decks from './store/modules/decks'
 import userDecks from './store/modules/user_decks'
+import users from './store/modules/users'
 
 Vue.use(Vuex)
 
@@ -12,36 +12,7 @@ export default new Vuex.Store({
   modules: {
     cards,
     decks,
-    userDecks
-  },
-  state: {
-    user: JSON.parse(localStorage.getItem('user')) || null
-  },
-  mutations: {
-    SET_USER(state, data) {
-      state.user = data
-      localStorage.setItem('user', JSON.stringify(data))
-    }
-  },
-  actions: {
-    logIn(context, user) {
-      axios
-        .post('/users/sign_in', user)
-        .then(response => {
-          context.commit('SET_USER', response.data)
-        })
-    },
-    signUp(context, user) {
-      axios
-        .post('/users', user)
-        .then(response => {
-          context.commit('SET_USER', response.data)
-        })
-    }
-  },
-  getters: {
-    loggedIn(state) {
-      return state.user && state.user.id
-    }
+    userDecks,
+    users
   }
 })
---
+++ app/javascript/store/modules/users.js
@@ -0,0 +1,42 @@
+import axios from 'axios'
+
+const state = {
+  user: JSON.parse(localStorage.getItem('user')) || null
+}
+
+const getters = {
+  loggedIn(state) {
+    return state.user && state.user.id
+  }
+}
+
+const actions = {
+  logIn(context, user) {
+    axios
+      .post('/users/sign_in', user)
+      .then(response => {
+        context.commit('SET_USER', response.data)
+      })
+  },
+  signUp(context, user) {
+    axios
+      .post('/users', user)
+      .then(response => {
+        context.commit('SET_USER', response.data)
+      })
+  }
+}
+
+const mutations = {
+  SET_USER(state, data) {
+    state.user = data
+    localStorage.setItem('user', JSON.stringify(data))
+  }
+}
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+}
