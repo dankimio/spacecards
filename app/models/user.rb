9206ea1c7045ec333ec85fd3efc98b950c73a7a4
Dan Kim <itsdanya@gmail.com>
2020-03-27 19:37:01 +0300

    Configure devise-jwt: - add :jwt_authenticable to User - use Rails secret_key_base for devise-jwt

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # TODO: use Blacklist revocation strategy
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: Devise::JWT::RevocationStrategies::Null
 end
