21666b07f632dc0e54360f55cf6cf09b1421817d
Dan Kim <itsdanya@gmail.com>
2020-03-28 22:17:22 +0300

    Test validity of User

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,6 +2,10 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   setup do
-    @user = users(:foo)
+    @user = users(:user)
+  end
+
+  test 'valid' do
+    assert @user.valid?
   end
 end
