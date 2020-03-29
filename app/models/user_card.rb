689324ecb4901e3cf70d9f293313075ff13b35dc
Dan Kim <itsdanya@gmail.com>
2020-03-29 14:21:44 +0300

    Add helper scopes to UserCard

--- app/models/user_card.rb
+++ app/models/user_card.rb
@@ -1,4 +1,7 @@
 class UserCard < ApplicationRecord
   belongs_to :user_deck
   belongs_to :shared_card, optional: true
+
+  scope :due, -> { where('due_at > ?', Time.zone.now) }
+  scope :fresh, -> { where(due_at: nil, recalled_at: nil, repetitions: 0) }
 end
