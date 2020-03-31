9266072558a906897d6497a91cb8d699294348d0
Dan Kim <itsdanya@gmail.com>
2020-03-31 20:44:11 +0300

    Include Pundit in ApplicationController, rescue from NotAuthorizedError

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
+  include Pundit
+
   protect_from_forgery unless: -> { request.format.json? }
 
   before_action :authenticate_user!
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def user_not_authorized
+    raise ActionController::RoutingError, 'Not Authorized'
+  end
 end
