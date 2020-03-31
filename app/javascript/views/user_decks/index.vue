f509e4989e38b86785517ad682407f27f58f39ff
Dan Kim <itsdanya@gmail.com>
2020-03-31 19:50:05 +0300

    Set title to 'My decks' in UserDecksIndex

--- app/javascript/views/user_decks/index.vue
+++ app/javascript/views/user_decks/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container md:max-w-xl lg:mx-auto">
     <h1 class="heading-2 mb-4 md:mb-8 md:text-center">
-      Decks
+      My decks
       <small>
         <router-link to="/user/decks/new" class="text-base ml-1">New deck</router-link>
       </small>
