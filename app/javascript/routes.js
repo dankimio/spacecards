db14eaad9e0622dfdf1920c773c6bea35563da9a
Dan Kim <itsdanya@gmail.com>
2020-03-31 21:17:34 +0300

    Move decks/edit to user_decks/edit

--- app/javascript/routes.js
+++ app/javascript/routes.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import DecksEdit from '@/views/decks/edit'
 import Explore from '@/views/explore/index'
 import Home from '@/views/home/index'
 import LogIn from '@/views/log_in/index'
 import SharedDecksShow from '@/views/shared_decks/show'
 import SignUp from '@/views/sign_up/index'
 import StudySessionShow from '@/views/study_session/show'
+import UserDecksEdit from '@/views/user_decks/edit'
 import UserDecksIndex from '@/views/user_decks/index'
 import UserDecksNew from '@/views/user_decks/new'
 import UserDecksShow from '@/views/user_decks/show'
@@ -20,11 +20,11 @@ export default new VueRouter({
     { path: '/sign-up', component: SignUp },
     { path: '/log-in', component: LogIn },
     { path: '/decks/:id', component: SharedDecksShow, props: true },
-    { path: '/decks/1/edit', component: DecksEdit },
     { path: '/explore', component: Explore },
     { path: '/user/decks', component: UserDecksIndex },
     { path: '/user/decks/new', component: UserDecksNew, props: true },
     { path: '/user/decks/:id', component: UserDecksShow, props: true },
+    { path: '/user/decks/:id/edit', component: UserDecksEdit, props: true },
     { path: '/user/decks/:id/study', component: StudySessionShow, props: true }
   ]
 })
