4b80a95087b84c372fd64cca18cb7d38700f6d71
Dan Kim <itsdanya@gmail.com>
2020-04-03 00:13:19 +0300

    UserDeck: add uniqueness validation for shared_deck

--- app/models/user_deck.rb
+++ app/models/user_deck.rb
@@ -20,6 +20,8 @@ class UserDeck < ApplicationRecord
   has_many :user_cards, dependent: :destroy
   has_many :study_sessions, dependent: :destroy
 
+  validates :shared_deck, uniqueness: { scope: :user }
+
   before_create :set_name_from_shared_deck, if: :shared_deck_id?
 
   after_create_commit :add_cards_from_shared_deck, if: :shared_deck_id?
