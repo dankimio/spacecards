32c306f63435872a2027b8b9ca81cca86c3c41a3
Dan Kim <itsdanya@gmail.com>
2020-04-07 18:46:14 +0300

    Extract HomeArticle component

---
+++ app/javascript/components/home/HomeArticle.vue
@@ -0,0 +1,38 @@
+<template>
+  <a
+    :href="url"
+    target="_blank"
+    class="article md:flex-1 flex flex-col
+          md:mx-4 mb-8 md:mb-0
+          border border-gray-200 rounded-lg overflow-hidden hover:shadow-lg"
+  >
+    <div class="flex-grow flex flex-col p-5 bg-white">
+      <p class="mb-2 text-sm">
+        <span class="text-gray-600 text-sm">{{ datePublished }} —</span>
+        <span class="text-gray-700 text-sm font-medium">
+          {{ publisher }}
+        </span>
+      </p>
+
+      <h3 class="font-medium text-xl mb-3">
+        {{ title }}
+      </h3>
+
+      <p class="text-gray-700 font-light leading-normal">
+        {{ excerpt }}
+      </p>
+    </div>
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    url: { type: String, required: true },
+    datePublished: { type: String, required: true },
+    publisher: { type: String, required: true },
+    title: { type: String, required: true },
+    excerpt: { type: String, required: true }
+  }
+}
+</script>
--- app/javascript/components/home/SectionArticles.vue
+++ app/javascript/components/home/SectionArticles.vue
@@ -10,64 +10,41 @@
       </p>
 
       <div class="flex flex-col md:flex-row">
-        <a
-          href="https://qz.com/1211561/how-to-learn-a-language-use-spaced-repetition/"
-          target="_blank"
-          class="article md:flex-1 flex flex-col
-          md:mx-4 mb-8 md:mb-0
-          border border-gray-200 rounded-lg overflow-hidden hover:shadow-lg"
-        >
-          <div class="flex-grow flex flex-col p-5 bg-white">
-            <p class="mb-2 text-sm">
-              <span class="text-gray-600 text-sm">Feb 23, 2018 — </span>
-              <span class="text-gray-700 text-sm font-medium">Quartz</span>
-            </p>
+        <HomeArticle
+          url="https://qz.com/1211561/how-to-learn-a-language-use-spaced-repetition/"
+          date-published="Feb 23, 2018"
+          publisher="Quartz"
+          title="The scientific, efficient way to learn languages: “spaced repetition”"
+          excerpt="Learning a language can be frustrating. Progress often feels slow. Part of that is because there is a lot to learn. But part of it is also that language education often does not utilize the best methods out there."
+        />
 
-            <h3 class="font-medium text-xl mb-3">The scientific, efficient way to learn languages: “spaced repetition”</h3>
+        <HomeArticle
+          url="https://www.theguardian.com/education/2016/jan/23/spaced-repetition-a-hack-to-make-your-brain-store-information"
+          date-published="Jan 23, 2016"
+          publisher="The Guardian"
+          title="Spaced repetition: a hack to make your brain store information"
+          excerpt="The way we think about health has been revolutionised by our growing evidence of how the body works. Yet the way most of us approach learning remains the same as it has for centuries."
+        />
 
-            <p class="text-gray-700 font-light leading-normal">Learning a language can be frustrating. Progress often feels slow. Part of that is because there is a lot to learn. But part of it is also that language education often does not utilize the best methods out there.</p>
-          </div>
-        </a>
-        <a
-          href="https://www.theguardian.com/education/2016/jan/23/spaced-repetition-a-hack-to-make-your-brain-store-information"
-          target="_blank"
-          class="article md:flex-1 flex flex-col
-          md:mx-4 mb-8 md:mb-0
-          border border-gray-200 rounded-lg overflow-hidden hover:shadow-lg"
-        >
-          <div class="flex-grow flex flex-col p-5 bg-white">
-            <p class="mb-2 text-sm">
-              <span class="text-gray-600">Jan 23, 2016 — </span>
-              <span class="text-gray-700 font-medium">The Guardian</span>
-            </p>
-
-            <h3 class="font-medium text-xl mb-3">Spaced repetition: a hack to make your brain store information </h3>
-
-            <p class="text-gray-700 font-light leading-normal">The way we think about health has been revolutionised by our growing evidence of how the body works. Yet the way most of us approach learning remains the same as it has for centuries.</p>
-          </div>
-        </a>
-        <a
-          href="https://fs.blog/2018/12/spacing-effect/"
-          target="_blank"
-          class="article md:flex-1 flex flex-col
-          md:mx-4 mb-8 md:mb-0
-          border border-gray-200 rounded-lg overflow-hidden hover:shadow-lg"
-        >
-          <div class="flex-grow flex flex-col p-5 bg-white">
-            <p class="mb-2 text-sm">
-              <span class="text-gray-600 text-sm">Dec 18, 2018 — </span>
-              <span class="text-gray-700 text-sm font-medium">Farnam Street</span>
-            </p>
-
-            <h3 class="font-medium text-xl mb-3">The Spacing Effect: How to Improve Learning and Maximize Retention</h3>
-
-            <p class="text-gray-700 font-light leading-normal">We are not taught how to learn in school, we are taught how to pass tests. The spacing effect is a far more effective way to learn and retain information that works with our brain instead of against it.</p>
-          </div>
-        </a>
+        <HomeArticle
+          url="https://fs.blog/2018/12/spacing-effect/"
+          date-published="Dec 18, 2018"
+          publisher="Farnam Street"
+          title="The Spacing Effect: How to Improve Learning and Maximize Retention"
+          excerpt="We are not taught how to learn in school, we are taught how to pass tests. The spacing effect is a far more effective way to learn and retain information that works with our brain instead of against it."
+        />
       </div>
     </div>
   </div>
 </template>
+
+<script>
+import HomeArticle from './HomeArticle'
+
+export default {
+  components: { HomeArticle }
+}
+</script>
 
 <style scoped>
 .article {
