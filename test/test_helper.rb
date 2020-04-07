241704f2b9577898f80c4d101831005296783bf1
Dan Kim <itsdanya@gmail.com>
2020-04-07 16:59:25 +0300

    Add jwt_headers test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,8 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+require 'devise/jwt/test_helpers'
+
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
@@ -10,4 +12,11 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  def jwt_headers(user = users(:user))
+    headers = {
+      'Accept' => 'application/json',
+      'Content-Type' => 'application/json'
+    }
+    Devise::JWT::TestHelpers.auth_headers(headers, user)
+  end
 end
