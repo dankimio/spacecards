77cc7642672c15f2dc6571d72716a2b659d60786
Dan Kim <itsdanya@gmail.com>
2020-04-18 20:10:18 +0300

    Add meta_description helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def meta_description(custom_description = nil, limit: 200)
+    page_description = custom_description ||
+                       t("#{current_translation_path}.meta_description", default: '')
+
+    if page_description.present?
+      page_description.truncate(limit)
+    else
+      t('layouts.application.meta_description').truncate(limit)
+    end
+  end
+
+  private
+
+  def current_translation_path
+    # `company/jobs` => `company.jobs`
+    namespace_and_controller = controller_path.gsub('/', '.')
+    # `company.jobs.index`
+    "#{namespace_and_controller}.#{action_name}"
+  end
 end
