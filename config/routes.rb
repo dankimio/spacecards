66b869bcf39d112a57444ee35df6afd598ffea73
Dan Kim <itsdanya@gmail.com>
2020-04-18 20:11:39 +0300

    Add shared_decks#index and shared_decks#show pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   root 'pages#index'
 
+  # Pages
+
+  get 'explore', to: 'shared_decks#index'
+  get 'decks/:id', to: 'shared_decks#show'
+
+  # API
+
   devise_for :users, controllers: {
     registrations: 'users/registrations',
     sessions: 'users/sessions'
@@ -17,7 +24,7 @@ Rails.application.routes.draw do
     resources :user_cards, only: %i[index create update destroy], shallow: true
   end
 
-  # Catchall route for history mode
+  # Catch-all route for history mode
   # https://router.vuejs.org/guide/essentials/history-mode.html#example-server-configurations
   get '/*path', to: 'pages#index'
 end
