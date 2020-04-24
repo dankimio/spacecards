e4132039f371b7292d0d42f49061d4af4baa1225
Dan Kim <itsdanya@gmail.com>
2020-04-25 02:01:31 +0300

    Clean up UserDeck layout

--- app/javascript/components/UserDeck.vue
+++ app/javascript/components/UserDeck.vue
@@ -3,26 +3,16 @@
     class="flex flex-col justify-between mb-5 border border-gray-200 rounded duration-100 transform hover:shadow-lg"
   >
     <div class="py-4 px-5">
-      <div class="text-lg md:mb-2">
+      <div class="text-lg md:text-xl">
         <router-link :to="{ name: 'userDeck', params: { id: userDeck.id.toString() } }">
           {{ userDeck.name }}
         </router-link>
       </div>
-      <router-link
-        :to="{ name: 'userDeck', params: { id: userDeck.id.toString() } }"
-        class="flex items-center text-sm text-gray-500 hover:text-indigo-600 flex-shrink-0 mr-2"
-      >
-        <inline-svg
-          :src="require('@images/icon-albums.svg')"
-          class="w-4 h-4 mr-1 fill-current"
-        />
-        {{ userDeck.userCardsCount }} cards
-      </router-link>
     </div>
 
-    <hr>
+    <hr class="border-gray-200">
 
-    <div class="flex text-sm bg-gray-100 py-2 px-5">
+    <div class="bg-gray-100 flex text-sm py-2 px-5">
       <router-link
         :to="{ name: 'studySession', params: { id: userDeck.id.toString() } }"
         class="flex items-center mr-5 text-gray-500 hover:text-indigo-600"
@@ -39,10 +29,10 @@
         class="flex items-center mr-5 text-gray-500 hover:text-indigo-600"
       >
         <inline-svg
-          :src="require('@images/icon-list.svg')"
-          class="inline-block w-4 h-4 mr-1 stroke-current"
+          :src="require('@images/icon-albums.svg')"
+          class="inline-block w-4 h-4 mr-1 fill-current"
         />
-        Browse
+        Cards
       </router-link>
 
       <router-link
