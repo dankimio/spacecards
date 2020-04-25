de2a362d4a4d5a35eda9b5d4191601af479f5167
Dan Kim <itsdanya@gmail.com>
2020-04-25 19:23:53 +0300

    Disable centering in user_decks/show

--- app/javascript/views/user_decks/show.vue
+++ app/javascript/views/user_decks/show.vue
@@ -1,77 +1,78 @@
 <template>
-  <div class="container flex">
-    <div class="w-full md:w-2/3 mx-auto">
-      <h1 class="text-3xl mb-2 md:mb-4">
-        {{ userDeck.name }}
+  <div class="container">
+    <h1 class="heading-2 mb-4 md:mb-8">
+      {{ userDeck.name }}
 
-        <small>
-          <router-link
-            v-if="userDeck.id"
-            :to="{ name: 'editUserDeck', params: { id: userDeck.id.toString() } }"
-            class="text-base ml-1"
-          >
-            Settings
-          </router-link>
-        </small>
-      </h1>
+      <small>
+        <router-link
+          v-if="userDeck.id"
+          :to="{ name: 'editUserDeck', params: { id: userDeck.id.toString() } }"
+          class="text-base ml-1"
+        >
+          Settings
+        </router-link>
+      </small>
+    </h1>
+    <div class="lg:grid grid-cols-3 gap-8">
+      <div class="col-start-3 row-start-1">
+        <router-link
+          v-if="userDeck.id"
+          :to="{ name: 'studySession', params: { id: userDeck.id.toString() } }"
+          class="button button-lg button-primary w-full uppercase tracking-wide mb-6"
+        >
+          Study
+        </router-link>
 
-      <p class="text-lg text-gray-700 mb-5 max-w-xl">
-        {{ userDeck.description }}
-      </p>
-
-      <div class="bg-gray-100 px-5 py-4 rounded mb-6 flex">
-        <div class="w-1/2">
-          <p class="mb-1 text-gray-600 uppercase tracking-wide text-sm">
-            Due cards
-          </p>
-          <span class="text-xl font-semibold text-gray-700">
-            {{ userDeck.dueCardsCount }}
-          </span>
+        <div class="bg-gray-100 px-5 py-4 rounded mb-4 flex">
+          <div class="w-1/2">
+            <p class="mb-1 text-gray-600 uppercase tracking-wide text-sm">
+              Due cards
+            </p>
+            <span class="text-xl font-semibold text-gray-700">
+              {{ userDeck.dueCardsCount }}
+            </span>
+          </div>
+          <div class="w-1/2">
+            <p class="mb-1 text-gray-600 uppercase tracking-wide text-sm">
+              New cards
+            </p>
+            <span class="text-xl font-semibold text-gray-700">
+              {{ userDeck.newCardsCount }}
+            </span>
+          </div>
         </div>
-        <div class="w-1/2">
-          <p class="mb-1 text-gray-600 uppercase tracking-wide text-sm">
-            New cards
-          </p>
-          <span class="text-xl font-semibold text-gray-700">
-            {{ userDeck.newCardsCount }}
-          </span>
-        </div>
+
+        <p class="text-lg text-gray-700 mb-5 max-w-xl">
+          {{ userDeck.description }}
+        </p>
       </div>
 
-      <router-link
-        v-if="userDeck.id"
-        :to="{ name: 'studySession', params: { id: userDeck.id.toString() } }"
-        class="button button-lg button-primary w-full uppercase tracking-wide mb-6"
-      >
-        Study
-      </router-link>
+      <div class="col-span-2 col-start-1 row-start-1">
+        <div
+          class="button button-outlined button-lg
+          w-full mb-6 uppercase tracking-wide text-gray-600 border-gray-400"
+          @click="addingNewCard = !addingNewCard"
+        >
+          Add cards
+        </div>
 
-      <hr class="mb-6 border-b-2 border-gray-200">
+        <DeckCard
+          v-if="addingNewCard"
+          :user-deck-id="userDeck.id"
+          :card="{ front: '', back: '' }"
+          :allow-editing="true"
+          :is-new="true"
+          @create="addingNewCard = false"
+          @cancel="addingNewCard = false"
+        />
 
-      <div
-        class="button button-outlined button-lg
-          w-full mb-5 uppercase tracking-wide text-gray-600 border-gray-400"
-        @click="addingNewCard = !addingNewCard"
-      >
-        Add cards
+        <DeckCard
+          v-for="userCard in userCards"
+          :key="userCard.id"
+          :card="userCard"
+          :allow-editing="true"
+        />
       </div>
-
-      <DeckCard
-        v-if="addingNewCard"
-        :user-deck-id="userDeck.id"
-        :card="{ front: '', back: '' }"
-        :allow-editing="true"
-        :is-new="true"
-        @create="addingNewCard = false"
-        @cancel="addingNewCard = false"
-      />
-
-      <DeckCard
-        v-for="userCard in userCards"
-        :key="userCard.id"
-        :card="userCard"
-        :allow-editing="true"
-      />
     </div>
   </div>
 </template>
