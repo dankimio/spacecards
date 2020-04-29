c2f26b773759cf41093def67da815866afc78f4a
Dan Kim <itsdanya@gmail.com>
2020-04-29 23:32:41 +0300

    Adjust width and height in UserDeckLoader

--- app/javascript/components/UserDeckLoader.vue
+++ app/javascript/components/UserDeckLoader.vue
@@ -2,17 +2,21 @@
 <template>
   <div class="flex flex-col border border-gray-200 rounded">
     <div class="py-4 px-5">
-      <ContentLoader :height="20">
-        <rect x="0" y="0" rx="3" ry="3" width="128" height="18" />
-      </ContentLoader>
+      <div class="mb-1" style="width: 128px; height: 24px">
+        <ContentLoader :width="128" :height="24">
+          <rect x="0" y="0" rx="3" ry="3" width="128" height="24" />
+        </ContentLoader>
+      </div>
     </div>
 
     <hr class="border-gray-200">
 
     <div class="bg-gray-100 flex text-sm py-2 px-5">
-      <ContentLoader :height="14">
-        <rect x="0" y="0" rx="3" ry="3" width="192" height="14" />
-      </ContentLoader>
+      <div style="width: 192px; height: 16px">
+        <ContentLoader :width="192" :height="16">
+          <rect x="0" y="0" rx="3" ry="3" width="192" height="16" />
+        </ContentLoader>
+      </div>
     </div>
   </div>
 </template>
