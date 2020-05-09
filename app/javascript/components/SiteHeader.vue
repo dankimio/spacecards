f7e47da5ee26a2311b4c72d875b5bc0b09f5f300
Dan Kim <itsdanya@gmail.com>
2020-05-09 15:02:38 +0300

    Mobile header

--- app/javascript/components/SiteHeader.vue
+++ app/javascript/components/SiteHeader.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="border-b border-gray-200">
-    <div class="container flex flex-col sm:flex-row sm:justify-between sm:items-center py-3">
-      <div class="mb-4 sm:mb-0">
+    <div class="container flex flex-row justify-between items-center py-3">
+      <div class="">
         <router-link :to="{ name: 'root' }" class="flex items-center">
-          <img src="@images/logo-compact.svg" alt="Logo" class="h-6">
+          <img src="@images/logo-compact.svg" alt="Logo" class="h-8 md:h-6">
           <span class="hidden md:block ml-4 text-gray-900 font-semibold text-2xl tracking-tight">
             spacecards
           </span>
@@ -13,26 +13,26 @@
         <router-link
           v-if="!isLoggedIn"
           :to="{ name: 'login' }"
-          class="mr-5 md:mr-0 md:ml-8"
+          class="ml-5 md:ml-8"
         >
           Log in
         </router-link>
         <router-link
           :to="{ name: 'explore' }"
-          class="mr-5 md:mr-0 md:ml-8"
+          class="ml-5 md:ml-8"
         >
           Explore
         </router-link>
         <router-link
           v-if="isLoggedIn"
           :to="{ name: 'userDecks' }"
-          class="mr-5 md:mr-0 md:ml-8"
+          class="ml-5 md:ml-8"
         >
           My decks
         </router-link>
         <a
           v-if="isLoggedIn"
-          class="mr-5 md:mr-0 md:ml-8"
+          class="ml-5 md:ml-8"
           href=""
           @click.prevent="logOut"
         >
