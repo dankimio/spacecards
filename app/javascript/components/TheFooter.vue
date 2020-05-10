7564946efa3e780e16b3cf7d1ab1c5dc02cde943
Dan Kim <itsdanya@gmail.com>
2020-05-11 02:09:42 +0300

    Redesign footer

--- app/javascript/components/TheFooter.vue
+++ app/javascript/components/TheFooter.vue
@@ -1,16 +1,28 @@
 <template>
   <footer
-    v-if="false"
-    class="container mt-auto flex justify-between py-6 md:py-8 lg:py-10"
+    class="container mt-auto flex flex-col md:flex-row
+    justify-between py-6 md:py-8 lg:py-10"
   >
-    <div class="text-gray-800">
-      Made by <a href="https://dankim.io" target="_blank">@dankimio</a>
+    <div class="text-gray-700 mb-2 md:mb-0">
+      Learn something new today.
     </div>
 
     <div class="text-grey">
-      <ul class="list-reset">
-        <li class="inline-block ml-4">
-          <a href="#" class="text-grey">About</a>
+      <ul class="list-reset flex">
+        <li class="mr-4 md:mr-5 md:ml-5">
+          <router-link :to="{ name: 'root' }">
+            Home
+          </router-link>
+        </li>
+        <li class="mr-4 md:mr-5 md:ml-5">
+          <router-link :to="{ name: 'root' }">
+            Explore
+          </router-link>
+        </li>
+        <li class="mr-4 md:mr-5 md:ml-5">
+          <router-link :to="{ name: 'root' }">
+            About
+          </router-link>
         </li>
       </ul>
     </div>
