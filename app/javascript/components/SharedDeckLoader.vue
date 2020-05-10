817b8fb4fd0be826f771c10ce36cfb7f20bb6856
Dan Kim <itsdanya@gmail.com>
2020-05-11 01:53:30 +0300

    SharedDeck: update content loader

--- app/javascript/components/SharedDeckLoader.vue
+++ app/javascript/components/SharedDeckLoader.vue
@@ -1,17 +1,14 @@
 <!-- eslint-disable vue/max-attributes-per-line -->
 <template>
   <div>
-    <ContentLoader :height="90">
-      <rect x="0" y="0" rx="3" ry="3" width="192" height="18" />
-      <rect x="0" y="34" rx="3" ry="3" width="384" height="12" />
-      <rect x="0" y="56" rx="3" ry="3" width="384" height="12" />
-      <rect x="0" y="78" rx="3" ry="3" width="256" height="12" />
-    </ContentLoader>
-
-    <div class="border-b border-gray-100 my-3" />
-
-    <ContentLoader :height="18">
-      <rect x="0" y="0" rx="3" ry="3" width="64" height="18" />
+    <ContentLoader :height="120" :width="300">
+      <!-- Title -->
+      <rect x="0" y="4" rx="3" ry="3" width="128" height="18" />
+      <!-- Cards count -->
+      <rect x="0" y="34" rx="3" ry="3" width="48" height="14" />
+      <!-- Description -->
+      <rect x="0" y="60" rx="3" ry="3" width="256" height="14" />
+      <rect x="0" y="82" rx="3" ry="3" width="256" height="14" />
     </ContentLoader>
   </div>
 </template>
