04265930aae5e3e07f14ed27fe8babbaa086aa70
Dan Kim <itsdanya@gmail.com>
2020-05-11 21:24:11 +0300

    Render whitespace in StudyCard

--- app/javascript/components/StudyCard.vue
+++ app/javascript/components/StudyCard.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="flex flex-col justify-center items-center bg-gray-100 rounded p-4">
-    <span class="md:py-4 font-semibold text-3xl">
-      {{ card.front }}
-    </span>
+    <pre
+      class="md:py-4 font-semibold text-3xl"
+      v-text="card.front"
+    />
 
     <hr v-show="answerShown" class="w-full border-b-2 border-gray-200 my-4">
 
-    <span v-show="answerShown" class="md:py-4 md:py-6 font-semibold text-3xl">
-      {{ card.back }}
-    </span>
+    <pre
+      v-show="answerShown"
+      class="md:py-4 md:py-6 font-semibold text-3xl"
+      v-text="card.back"
+    />
   </div>
 </template>
 
